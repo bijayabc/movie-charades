d409b388056a6f4b5503d0bdf7e9937113f1991c
bijayabc <bijayabc1234@gmail.com>
2024-12-08 15:17:40 -0500

    first commit: ready to push project

---
+++ README.md
@@ -0,0 +1,34 @@
+# Dumb Charades Web App
+
+This is a web application for playing Dumb Charades, where users can select categories and get random titles (movies, stars, songs) to act out. The app also includes a 60 timer for each title and an option for users to send messages or suggestions.
+
+
+## Features
+
+- Select from different movie categories: Kollywood, Bollywood, Hollywood, Editor's Choice, and Random.
+- Display random titles from the selected category.
+- Built-in timer to keep track of game time.
+- Responsive design for different screen sizes.
+- Contact form to send messages or suggestions.
+- Links to social media profiles.
+
+## Technologies Used
+
+- HTML5
+- CSS3
+- Vanilla JavaScript
+- [Web3Forms](https://web3forms.com) for handling form submissions
+- Google Fonts
+
+## Usage
+
+1. Select a movie category from the options provided.
+2. The app will display a random movie name from the selected category.
+3. Use the built-in timer to keep track of the game time.
+4. Fill out the contact form to send any messages or suggestions.
+
+## Contact
+
+- [Instagram](https://www.instagram.com/bijay.bc/)
+- [GitHub](https://github.com/bijayabc)
+- [LinkedIn](https://www.linkedin.com/in/bijayabc/)
---
+++ script.js
@@ -0,0 +1,74 @@
+const toggleButton = document.getElementById("toggle-button")
+const navLinks = document.getElementsByClassName("nav-links")[0]
+
+toggleButton.addEventListener("click", () => {
+    navLinks.classList.toggle("active")
+}) 
+
+function generateSelection(choice) {
+    
+    // I created and used my own json file because I couldn't find a suitable API that gave me all the necessary movies by industry
+    fetch("data.json")
+        .then(response => response.json())
+        .then(data => {
+
+            let displayItem
+
+            if (choice !== "random") {
+                const randomIndex = Math.floor(Math.random() * data[choice].length)
+                displayItem =  data[choice][randomIndex]
+
+            } else {
+                const options = ["kollywood", "bollywood", "hollywood", "editor"]
+
+                const choiceIndex = Math.floor(Math.random() * 4)
+                const randomChoice = options[choiceIndex]
+    
+                const randomIndex = Math.floor(Math.random() * data[randomChoice].length)
+                displayItem =  data[randomChoice][randomIndex]
+            }
+            
+            const displayText = document.getElementsByClassName("display-text")[0]
+            displayText.innerHTML = displayItem
+
+            // timer display
+            startTimer(60)
+        })
+        .catch(error => {
+            console.log('Error fetching or parsing JSON:', error)
+        })
+}3
+
+let timerInterval = null; // Make it false so that the first timer isn't cleared and runs accurately
+function startTimer(seconds) {
+    if (timerInterval) {
+        clearInterval(timerInterval) // If a previous timer exists, stop it.
+    }
+    timerInterval = setInterval(timerCountdown, 1000)
+
+    function timerCountdown() {
+        const timer = document.getElementsByClassName("time")[0]
+        timer.innerHTML = "00: "+ seconds
+        seconds--
+
+        if (seconds < 0) {
+            clearInterval(timerInterval)
+            timer.innerHTML = "TIME'S UP"
+        }
+    }
+}
+
+nepaliMovie = document.getElementById("kollywood")
+nepaliMovie.addEventListener("click", () => generateSelection("kollywood"))
+
+hindiMovie = document.getElementById("bollywood")
+hindiMovie.addEventListener("click", () => generateSelection("bollywood"))
+
+englishMovie = document.getElementById("hollywood")
+englishMovie.addEventListener("click", () => generateSelection("hollywood"))
+
+editorChoice = document.getElementById("editor")
+editorChoice.addEventListener("click", () => generateSelection("editor"))
+
+randomOption = document.getElementById("random")
+randomOption.addEventListener("click", () => generateSelection("random"))
